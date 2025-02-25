0dc3d6ea89aacbee806ef910141c7b36a3024f11
SergeyDudinskiy <2sery@mail.ru>
2025-02-25 16:43:08 +0300

    Done test task

--- MainForm.cpp
+++ MainForm.cpp
@@ -1,16 +1,26 @@
-//---------------------------------------------------------------------------
-#include <vcl.h>
-#pragma hdrstop
-
-#include "MainForm.h"
+﻿#include "MainForm.h"
+#include "ChildForm.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+//---------------------------------------------------------------------------
 #pragma resource "*.dfm"
 
 TMainForm* MainForm;
-//---------------------------------------------------------------------------
+
 __fastcall TMainForm::TMainForm(TComponent* Owner) : TForm(Owner)
 {
 
 }
 //---------------------------------------------------------------------------
+void TMainForm::CreateChildForm(const String& fileName)
+{
+	TChildForm* ChildForm = new TChildForm(this);
+	ChildForm->LoadImage(fileName);
+    ChildForm->Show();
+}
+//---------------------------------------------------------------------------
+void __fastcall TMainForm::OpenItemClick(TObject* Sender)
+{
+	if (OpenDialog->Execute())
+		CreateChildForm(OpenDialog->FileName);
+}
--- Project5.cpp
+++ Project5.cpp
@@ -12,7 +12,6 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->Initialize();
 		Application->MainFormOnTaskBar = true;
 		Application->CreateForm(__classid(TMainForm), &MainForm);
-		Application->CreateForm(__classid(TChildForm), &ChildForm);
 		Application->Run();
 	}
 	catch (Exception& exception) {
